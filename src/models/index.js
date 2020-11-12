2330fddcf919fea107d13e8d2293b6bbb8b94136
Flones <46957418+Flones@users.noreply.github.com>
2020-11-12 09:04:11 +0100

    recipes updated

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+    //mongoose.connect('mongodb://localhost/chefclub', {
+    //mongoose.connect('mongodb+srv://Stephane:root@cluster0.bdu8e.mongodb.net/Mydatabase?retryWrites=true&w=majority', { //serv chez Nelson
+    //mongoose.connect('mongodb+srv://Flones:Br2NXMNmYJm4RA9X@cluster0.bdu8e.mongodb.net/MyDatabase?retryWrites=true&w=majority', { 
+    mongoose.connect('mongodb+srv://Flones:Br2NXMNmYJm4RA9X@cluster0.cdevd.mongodb.net/ChefClub?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDb is run');
+})
+
+module.exports = mongoose;
